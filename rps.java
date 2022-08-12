61976deb995f11e7c011bade09894526b61614b4
ritizzzz <kandelritiz@gmail.com>
2022-08-12 15:22:56 +1000

    initial commit

---
+++ rps.java
@@ -0,0 +1,8 @@
+public class rps{
+    public static void main(String[] args){
+        char[] moves = {'R', 'P', 'S'};
+        for(int i = 0; moves.length < 2; i++){
+            System.out.println(moves[i]);
+        }
+    }
+}
