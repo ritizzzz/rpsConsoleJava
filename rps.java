4930466d4f69b777fce3f8b2508ad3267e9e5dec
ritizzzz <kandelritiz@gmail.com>
2022-08-12 15:39:05 +1000

    computer move added

--- rps.java
+++ rps.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class rps{
     public static void main(String[] args){
         
@@ -7,15 +8,20 @@ public class rps{
 
         Scanner scnr = new Scanner(System.in);
 
+        System.out.println("Enter your move:");
         char userMove = scnr.next().charAt(0);
 
+        int randomInt = (int)(Math.random() * moves.length);
+
+        char computerMove = moves[randomInt];
+        
         for(int i = 0; i<moves.length; i++){
             if(Character.toUpperCase(userMove) == moves[i]){
                 validMove = true;
             }
         }
         
-        System.out.println(validMove);
+        System.out.println(computerMove);
         scnr.close();
     }
 }
