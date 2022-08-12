521102168adb67e12e8a4b9133e34ecc401b6795
ritizzzz <kandelritiz@gmail.com>
2022-08-12 15:30:59 +1000

    user validation added

--- rps.java
+++ rps.java
@@ -1,8 +1,21 @@
+import java.util.*;
 public class rps{
     public static void main(String[] args){
+        
         char[] moves = {'R', 'P', 'S'};
-        for(int i = 0; moves.length < 2; i++){
-            System.out.println(moves[i]);
+        boolean validMove = false;
+
+        Scanner scnr = new Scanner(System.in);
+
+        char userMove = scnr.next().charAt(0);
+
+        for(int i = 0; i<moves.length; i++){
+            if(Character.toUpperCase(userMove) == moves[i]){
+                validMove = true;
+            }
         }
+        
+        System.out.println(validMove);
+        scnr.close();
     }
 }
