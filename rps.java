874ea1ed699cc11656d5c0e30b4b593b45575392
ritizzzz <kandelritiz@gmail.com>
2022-08-14 21:28:10 +1000

    further validation to use input

--- rps.java
+++ rps.java
@@ -1,27 +1,32 @@
 import java.util.*;
 
 public class rps{
+    public String playerWin(char playerMove, int computerMove){
+        char[] winningComb = {'P', 'S', 'R'};
+        char[] losingComb = {'S', 'R', 'P'};
+        
+        
+        return "tie";
+    }
     public static void main(String[] args){
         
-        char[] moves = {'R', 'P', 'S'};
-        boolean validMove = false;
+        String moves = "RPS";
 
         Scanner scnr = new Scanner(System.in);
 
         System.out.println("Enter your move:");
-        char userMove = scnr.next().charAt(0);
-
-        int randomInt = (int)(Math.random() * moves.length);
-
-        char computerMove = moves[randomInt];
+        String userMove = scnr.next().toUpperCase();
         
-        for(int i = 0; i<moves.length; i++){
-            if(Character.toUpperCase(userMove) == moves[i]){
-                validMove = true;
-            }
+        int compMove = (int)(Math.random() * moves.length());
+        int indexOfUserMove = moves.indexOf(userMove);
+    
+        while(indexOfUserMove == -1 || userMove.length() != 1){
+            System.out.println("Enter your move:");
+            userMove = scnr.next().toUpperCase();
+            indexOfUserMove = moves.indexOf(userMove);
         }
-        
-        System.out.println(computerMove);
+
+        System.out.println(indexOfUserMove);
         scnr.close();
     }
 }
