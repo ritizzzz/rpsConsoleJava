83a6375417839075540e080be374c4e2ec681ad9
ritizzzz <kandelritiz@gmail.com>
2022-08-14 22:19:43 +1000

    refactoring

--- rps.java
+++ rps.java
@@ -16,17 +16,28 @@ public class rps{
 
         System.out.println("Enter your move:");
         String userMove = scnr.next().toUpperCase();
+        outerloop:
+        while(!userMove.equals("Q")){
+            int compMove = (int)(Math.random() * moves.length());
+            int indexOfUserMove = moves.indexOf(userMove);
         
-        int compMove = (int)(Math.random() * moves.length());
-        int indexOfUserMove = moves.indexOf(userMove);
-    
-        while(indexOfUserMove == -1 || userMove.length() != 1){
+            while(indexOfUserMove == -1 || userMove.length() != 1){
+                System.out.println("Move Invalid!!!");
+                System.out.println();
+                System.out.println("Enter your move:");
+                userMove = scnr.next().toUpperCase();
+                indexOfUserMove = moves.indexOf(userMove);
+                if(userMove.equals("Q")){
+                    break outerloop;
+                }
+            }
+            System.out.printf("Your move is: %s\n", userMove);
+            System.out.printf("Computers move is: %s\n\n", moves.substring(compMove, compMove+1));
+            
             System.out.println("Enter your move:");
-            userMove = scnr.next().toUpperCase();
-            indexOfUserMove = moves.indexOf(userMove);
+            userMove = scnr.next().toUpperCase();    
         }
 
-        System.out.println(indexOfUserMove);
         scnr.close();
     }
 }
